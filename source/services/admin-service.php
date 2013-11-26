ba490cc8ea44f5f0c4fb9860bf0c8a095dd1f37e
sanh23021987@gmail.com <sanh23021987@gmail.com@da613bb2-ad83-1ab1-e056-9c75d5d3a260>
2013-11-26 07:52:51 +0000

    - add login panel

---
+++ source/services/admin-service.php
@@ -0,0 +1,62 @@
+﻿<?php
+// include all BLL use to handlen request
+include '../DAO/connection.php';
+include '../DTO/object.php';
+include '../BLL/categoryBll.php';
+include '../BLL/foodBll.php';
+include '../BLL/mealTableBll.php';
+include '../BLL/orderDetailBll.php';
+include '../BLL/ordersBll.php';
+include '../BLL/roleBll.php';
+include '../BLL/staffBll.php';
+include '../BLL/tempOrderDetailBll.php';
+// Handle for each request should add in two line comment
+
+// ---------------- REQUEST WITH FLAG : loadHorizontalTabs --------------------
+$flag = $_POST['flag'];
+
+if($flag == 'getAllCategories'){
+    $categories = getAllCategories();
+    // return value which function call ajax receive response
+    echo json_encode($categories);
+} else if($flag == 'getAllFoods'){
+    $foods = getAllFoods();
+    echo json_encode($foods);
+} else if($flag == 'getAllStaffs'){
+    $staffs = getAllStaffs();
+    echo json_encode($staffs);
+} else if($flag == 'getAllRoles'){
+    $roles = getAllRoles();
+    echo json_encode($roles);
+} else if($flag == 'addCategorie'){
+    $name = $_POST['name'];
+    $description = $_POST['description'];
+    $status = insertCategory($name, $description);
+    if($status == -1){
+        echo 'fail';
+    } else {
+        echo 'success';
+    }
+} else if($flag == 'editCategorie'){
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $description = $_POST['description'];
+    $status = updateCategoryById($id, $name, $description);
+    if($status == -1){
+        echo 'fail';
+    } else {
+        echo 'success';
+    }
+} else if($flag == 'deleteCategorie'){
+    $id = $_POST['id'];
+    $status = deleteCategoryById($id);
+    if($status == -1){
+        echo 'fail';
+    } else {
+        echo 'success';
+    }
+}
+
+// ---------------- END REQUEST WITH FLAG : loadHorizontalTabs ----------------
+
+?>
---
+++ source/services/service.php
@@ -0,0 +1,26 @@
+﻿<?php
+// include all BLL use to handlen request
+include '../DAO/connection.php';
+include '../DTO/object.php';
+include '../BLL/categoryBll.php';
+include '../BLL/foodBll.php';
+include '../BLL/mealTableBll.php';
+include '../BLL/orderDetailBll.php';
+include '../BLL/ordersBll.php';
+include '../BLL/roleBll.php';
+include '../BLL/staffBll.php';
+include '../BLL/tempOrderDetailBll.php';
+// Handle for each request should add in two line comment
+
+// ---------------- REQUEST WITH FLAG : loadHorizontalTabs --------------------
+$flag = $_POST['flag'];
+
+if($flag == 'loadHorizontalMenu'){
+    $categorys = getAllCategory();
+	// return value which function call ajax receive response
+    echo json_encode($categorys);
+}
+
+// ---------------- END REQUEST WITH FLAG : loadHorizontalTabs ----------------
+
+?>
