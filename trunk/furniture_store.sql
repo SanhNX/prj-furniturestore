5a9626cd7d3eb8dc9a81f85df7971d92a17bfc9f
sanh23021987@gmail.com <sanh23021987@gmail.com@da613bb2-ad83-1ab1-e056-9c75d5d3a260>
2013-11-26 07:54:13 +0000

    - update db

--- trunk/furniture_store.sql
+++ trunk/furniture_store.sql
@@ -7,6 +7,14 @@ create table tbl_category
     name varchar(100) not null,
     description varchar(1000)
 );
+create table tbl_sub_category
+(
+    id int auto_increment primary key not null,
+    cateid int not null,
+    name varchar(100) not null,
+    description varchar(1000),
+    CONSTRAINT FOREIGN KEY(cateid) references tbl_category(id)
+);
 create table tbl_product
 (
     id int auto_increment primary key not null,
@@ -25,10 +33,7 @@ create table tbl_administrator
     id int auto_increment primary key not null,
     name varchar(30) not null,
     email varchar(50) not null,
-    password varchar(50) not null,
-    avatar varchar(100),
-    gender int,
-    status int
+    password varchar(50) not null
 );
 create table tbl_feedback
 (
@@ -38,3 +43,31 @@ create table tbl_feedback
     email varchar(50) not null,
     description varchar(1000) not null
 );
+INSERT INTO tbl_administrator(name, email, password) VALUES('Administrator', 'admin@gmail.com', 'admin');
+
+
+INSERT INTO tbl_category(name) VALUES('Nội thất gia đình');
+INSERT INTO tbl_category(name) VALUES('Nội thất văn phòng');
+INSERT INTO tbl_category(name) VALUES('Đồ gỗ ngoài trời');
+
+INSERT INTO tbl_sub_category(cateid, name) VALUES(1, 'Bàn ăn');
+INSERT INTO tbl_sub_category(cateid, name) VALUES(1, 'Ghế');
+INSERT INTO tbl_sub_category(cateid, name) VALUES(1, 'Giường');
+INSERT INTO tbl_sub_category(cateid, name) VALUES(1, 'Tủ áo');
+INSERT INTO tbl_sub_category(cateid, name) VALUES(1, 'Salon');
+INSERT INTO tbl_sub_category(cateid, name) VALUES(1, 'Kệ tivi');
+INSERT INTO tbl_sub_category(cateid, name) VALUES(1, 'Tủ ngăn phòng');
+INSERT INTO tbl_sub_category(cateid, name) VALUES(1, 'Tủ rượu');
+INSERT INTO tbl_sub_category(cateid, name) VALUES(1, 'Tủ đầu giường');
+INSERT INTO tbl_sub_category(cateid, name) VALUES(1, 'Tủ đa năng');
+INSERT INTO tbl_sub_category(cateid, name) VALUES(1, 'Kệ giày');
+INSERT INTO tbl_sub_category(cateid, name) VALUES(1, 'Bàn trang điểm');
+INSERT INTO tbl_sub_category(cateid, name) VALUES(1, 'Bàn học');
+
+INSERT INTO tbl_sub_category(cateid, name) VALUES(2, 'Bàn làm việc');
+INSERT INTO tbl_sub_category(cateid, name) VALUES(2, 'Bàn lãnh đạo');
+INSERT INTO tbl_sub_category(cateid, name) VALUES(2, 'Bàn họp');
+INSERT INTO tbl_sub_category(cateid, name) VALUES(2, 'Tủ hồ sơ');
+INSERT INTO tbl_sub_category(cateid, name) VALUES(2, 'Bục kệ');
+
+INSERT INTO tbl_sub_category(cateid, name) VALUES(3, 'Đồ gỗ ngoài trời');
