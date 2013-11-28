12ffabf251f06158e7b7ea26f2b7973da1d5ded2
sanh23021987@gmail.com <sanh23021987@gmail.com@da613bb2-ad83-1ab1-e056-9c75d5d3a260>
2013-11-28 07:32:06 +0000

    - code category, sub category manage function

--- source/services/admin-service.php
+++ source/services/admin-service.php
@@ -3,6 +3,7 @@
 include '../DAO/connection.php';
 include '../DTO/object.php';
 include '../BLL/categoryBll.php';
+include '../BLL/subCategoryBll.php';
 // Handle for each request should add in two line comment
 
 // ---------------- REQUEST WITH FLAG : loadHorizontalTabs --------------------
@@ -12,16 +13,11 @@ if($flag == 'getAllCategories'){
     $categories = getAllCategories();
     // return value which function call ajax receive response
     echo json_encode($categories);
-} else if($flag == 'getAllFoods'){
-    $foods = getAllFoods();
-    echo json_encode($foods);
-} else if($flag == 'getAllStaffs'){
-    $staffs = getAllStaffs();
-    echo json_encode($staffs);
-} else if($flag == 'getAllRoles'){
-    $roles = getAllRoles();
-    echo json_encode($roles);
-} else if($flag == 'addCategorie'){
+} else if($flag == 'loadItemCategorie') {
+    $categorie = getCategoryById($_POST['id']);
+    // return value which function call ajax receive response
+    echo json_encode($categorie);
+}else if($flag == 'addCategorie'){
     $name = $_POST['name'];
     $description = $_POST['description'];
     $status = insertCategory($name, $description);
@@ -30,7 +26,7 @@ if($flag == 'getAllCategories'){
     } else {
         echo 'success';
     }
-} else if($flag == 'editCategorie'){
+} else if($flag == 'updateCategorie'){
     $id = $_POST['id'];
     $name = $_POST['name'];
     $description = $_POST['description'];
@@ -43,6 +39,45 @@ if($flag == 'getAllCategories'){
 } else if($flag == 'deleteCategorie'){
     $id = $_POST['id'];
     $status = deleteCategoryById($id);
+    if($status == -1){
+        echo 'fail';
+    } else {
+        echo 'success';
+    }
+} 
+// ------------------ SUB CATEGORY --------------------------
+else if($flag == 'getAllSubCategoriesByCateId'){
+    $categories = getAllSubCategoriesByCateId($_POST['cateId']);
+    // return value which function call ajax receive response
+    echo json_encode($categories);
+} else if($flag == 'loadItemSubCategorie') {
+    $categorie = getSubCategoryById($_POST['id']);
+    // return value which function call ajax receive response
+    echo json_encode($categorie);
+}else if($flag == 'addSubCategorie'){
+    $name = $_POST['name'];
+    $cateId = $_POST['cateId'];
+    $description = $_POST['description'];
+    $status = insertSubCategory($cateId, $name, $description);
+    if($status == -1){
+        echo 'fail';
+    } else {
+        echo 'success';
+    }
+} else if($flag == 'updateSubCategorie'){
+    $id = $_POST['id'];
+    $cateId = $_POST['cateId'];
+    $name = $_POST['name'];
+    $description = $_POST['description'];
+    $status = updateSubCategoryById($id, $cateId, $name, $description);
+    if($status == -1){
+        echo 'fail';
+    } else {
+        echo 'success';
+    }
+} else if($flag == 'deleteSubCategorie'){
+    $id = $_POST['id'];
+    $status = deleteSubCategoryById($id);
     if($status == -1){
         echo 'fail';
     } else {
